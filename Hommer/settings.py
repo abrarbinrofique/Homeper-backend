6514b0fd93e5b4bcad021effc61631d1002027da
abrarbinrofique <abrar46@student.sust.edu>
2024-08-08 22:41:24 +0600

    gdfsdgsd

--- Hommer/settings.py
+++ Hommer/settings.py
@@ -71,13 +71,32 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 # CORS settings
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",
-    "https://homeper.onrender.com",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:5500",
+#     "https://homeper.onrender.com",
+# ]
 CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOW_CREDENTIALS = True
-
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 ROOT_URLCONF = 'Hommer.urls'
 
 TEMPLATES = [
