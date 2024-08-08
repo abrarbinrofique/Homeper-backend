5e8812a9fcc291624cebe0bd05c305703f517754
abrarbinrofique <abrar46@student.sust.edu>
2024-08-08 21:33:46 +0600

    settings update

--- Hommer/settings.py
+++ Hommer/settings.py
@@ -71,12 +71,12 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 # CORS settings
-# CORS_ALLOWED_ORIGINS = [
-#     "http://127.0.0.1:5500",
-#     "https://homeper.onrender.com",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "https://homeper.onrender.com",
+]
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'Hommer.urls'
 
