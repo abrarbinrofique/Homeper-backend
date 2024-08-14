58767af819d2b911ee3038f10cc3aa850af37d5a
abrarbinrofique <abrar46@student.sust.edu>
2024-08-14 17:39:20 +0600

    cloudinary update

--- Hommer/settings.py
+++ Hommer/settings.py
@@ -204,11 +204,10 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = env("EMAIL")
 EMAIL_HOST_PASSWORD =env('EMAIL_PASSWORD')
 
-CLOUDINARY_STORAGE={
-
-    'CLOUD_NAME':'dk2vgd0dv',
-    'API_KEY':'585292437313575',
-    'API_SECRET':'C5MqCGPlWqcc6tgKkmGKwacBiCY'
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env('CLOUD_NAME'),
+    'API_KEY': env('API_KEY'),
+    'API_SECRET': env('API_SECRET'),
 }
 
-DEFAULT_FILE_STORAGE='cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
