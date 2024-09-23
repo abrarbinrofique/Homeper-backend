33334736c645da4fa59a0f3a860882cb49772d7e
abrarbinrofique <abrar46@student.sust.edu>
2024-09-23 13:08:46 +0600

    backend update

--- customer/urls.py
+++ customer/urls.py
@@ -10,7 +10,7 @@ router.register('', views.CustomerViewsets)
 
 urlpatterns = [
     path('userlist/',views.UserViewSet.as_view({'get': 'list'}), name='userlist'),
-    path('login/',views.UserLoginAPIView.as_view(),name='login'),
+    path('login/',views.UserLoginAPIView.as_view(template_name='login.html'),name='login'),
     path('logout/',views.UserLogoutview.as_view(),name='logout'),
     path('register/',views.UserRegistraitionView.as_view(),name='register'),
     path('active/<uid64>/<token>/',views.activate,name="active"),
