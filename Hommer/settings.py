68be18c8e1d201ddf49cbdae618e85c22b830a36
abrarbinrofique <abrar46@student.sust.edu>
2024-09-24 02:32:27 +0600

    problem of tonight

--- Hommer/settings.py
+++ Hommer/settings.py
@@ -157,7 +157,6 @@ STATIC_ROOT=BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
